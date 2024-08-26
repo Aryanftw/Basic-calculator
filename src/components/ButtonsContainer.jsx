8c2628c181d700294de4d157bfd9616eb0996db4
Aryanftw <ap890383@gmail.com>
2024-08-26 11:24:44 +0530

    first commit

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -2,18 +2,6 @@ import styles from './ButtonsContainer.module.css'
 
 function ButtonsContainer({onButtonClick}){
   const buttonName = ['C','1','2','+','3','4','-','5','6','*','7','8','/','=','9','0','.',]
-
-//   return(
-  
-//   <div className={styles.buttonContainer}>
-
-//       {buttonName.map((name) => (<button className={styles.button} onclick = {() => {
-//         onButtonClick(buttonName)
-//       >{name}</button>))};
-//     </div>
-  
-//   );
-// }
 return(
   
   <div className={styles.buttonContainer}>
